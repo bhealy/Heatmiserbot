bd2f00ecd4914dcb2121a3d6aff19e196340367e
Bobby Healy <43304069+bhealy@users.noreply.github.com>
2023-02-06 16:02:47 +0000

    made offlien notif less noisy
    Fires max once every 10 mins

--- src/heatmiserbot/Switch.java
+++ src/heatmiserbot/Switch.java
@@ -2,14 +2,16 @@ package heatmiserbot;
 
 import com.google.gson.Gson;
 import com.google.gson.internal.LinkedTreeMap;
+import java.time.LocalTime;
 import java.util.HashMap;
 
 public class Switch {
 
-        class SwitchResult {
+    class SwitchResult {
 
         boolean result;
     }
+
     /**
      * Sets the tuya switch controlling pump to on/off
      *
@@ -18,28 +20,33 @@ public class Switch {
     public static void set(String deviceID, String accessID, String secret, boolean state) {
 
         TuyaSender.log("Setting switch to " + (state ? " On" : " Off"));
-        String error=null;
+        String error = null;
         try {
 
             TuyaSender.creds c = TuyaSender.getCreds(accessID, secret);  // request an access token
-            String path="/v1.0/iot-03/devices/" + deviceID + "/commands";
-            String body="{\"commands\":[{\"code\":\"switch_1\",\"value\":" + (state ? "true" : "false") + "}]}";
-            LinkedTreeMap result = (LinkedTreeMap)TuyaSender.execute(accessID, secret, c.token, path, "POST", body, new HashMap<>());
-            
-            TuyaSender.log("Result of switch control:"+result);
-            Boolean r=(Boolean)result.get("success");
-            if (r!=null && !r) 
-                error=String.valueOf(result.get("msg"));
-            if (r==null)
-                error="Unknwn (null) response from the command";
-            
+            String path = "/v1.0/iot-03/devices/" + deviceID + "/commands";
+            String body = "{\"commands\":[{\"code\":\"switch_1\",\"value\":" + (state ? "true" : "false") + "}]}";
+            LinkedTreeMap result = (LinkedTreeMap) TuyaSender.execute(accessID, secret, c.token, path, "POST", body, new HashMap<>());
+
+            TuyaSender.log("Result of switch control:" + result);
+            Boolean r = (Boolean) result.get("success");
+            if (r != null && !r) {
+                error = String.valueOf(result.get("msg"));
+            }
+            if (r == null) {
+                error = "Unknwn (null) response from the command";
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
-            error=e.toString();
+            error = e.toString();
         }
-        if (error!=null) {
+        if (error != null) {
             // error
-            Email.send(HeatMiserBot.emailUser,HeatMiserBot.emailPassword,"Heatmiserbot error switching pump to "+state,"Check Heatmiserbot running on iMac and check pump switch is accessible via Tuya / reset Tuya switch->"+error);
-        } 
+            // only send every 5 mins to reduce noise
+            if (LocalTime.now().getMinute() % 10 == 0 && LocalTime.now().getSecond()<50) { // <50 ensures a script called every min doesn't accidentally pass the %10 test twice in the same minute
+                Email.send(HeatMiserBot.emailUser, HeatMiserBot.emailPassword, "Heatmiserbot error switching pump to " + state, "Check Heatmiserbot running on iMac and check pump switch is accessible via Tuya / reset Tuya switch->" + error);
+            }
+        }
     }
 }
